d82ac7f41d5954a8e33cc028f8501d5fcd75fe04
Harsh Sharma <harshsharmaff56@gmail.com>
2026-07-29 13:18:19 +0530

    refactor: update routing and navbar to use react-router-dom

--- Habitflow/src/App.tsx
+++ Habitflow/src/App.tsx
@@ -1,14 +1,24 @@
 import Index from "./pages"
 import Navbar from "./components/navbar.tsx"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+
 function App() {
 
   return (
     <div >
+      <BrowserRouter>
       <Navbar />
-      <Index />
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/goals" element={<h1>Goals</h1>} />
+          <Route path="/calendar" element={<h1>Calendar</h1>} />
+          <Route path="/analytics" element={<h1>Analytics</h1>} />
+          <Route path="/settings" element={<h1>Settings</h1>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
-
 }
 
 export default App
--- Habitflow/src/components/navbar.tsx
+++ Habitflow/src/components/navbar.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 import {
   LayoutDashboard,
   Target,
@@ -7,8 +9,35 @@ import {
 } from "lucide-react";
 
 function Navbar() {
-  const nav = ["Dashboard", "Goals", "Calendar", "Analytics", "Settings"]
-  const icons = [LayoutDashboard, Target, CalendarDays, ChartColumn, Settings]
+
+const navItems = [
+  {
+    name: "Dashboard",
+    to: "/",
+    icon: LayoutDashboard,
+  },
+  {
+    name: "Goals",
+    to: "/goals",
+    icon: Target,
+  },
+  {
+    name: "Calendar",
+    to: "/calendar",
+    icon: CalendarDays,
+  },
+  {
+    name: "Analytics",
+    to: "/analytics",
+    icon: ChartColumn,
+  },
+  {
+    name: "Settings",
+    to: "/settings",
+    icon: Settings,
+  },
+];
+
   return (
     <nav className="bg-[#232729] border-b border-[#323638] px-8 py-4">
       <div className="max-w-7xl mx-auto flex items-center justify-between">
@@ -27,13 +56,13 @@ function Navbar() {
         {/* Navigation */}
         <div className="flex items-center gap-8">
           {
-            nav.map((item, index) => {
-              const Icon = icons[index]
+            navItems.map(({ name, to, icon: Icon }) => {
               return (
-                <button key={index} className="flex items-center gap-2 text-[#A7BF80] font-medium hover:text-[#FFFFFF] transition-colors duration-200 ease-in cursor-pointer">
+                <Link to={to}
+                  key={name} className="flex items-center gap-2 text-[#A7BF80] font-medium hover:text-[#FFFFFF] transition-colors duration-200 ease-in cursor-pointer" >
                   <Icon size={18} />
-                  {item}
-                </button>
+                  {name}
+                </Link>
               )
             })
           }
